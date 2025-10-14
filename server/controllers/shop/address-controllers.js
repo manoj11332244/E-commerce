ba379b689f1a311891ecb634c62789343327a738
Manoj <sahilku770@gmail.com>
2025-10-14 21:44:11 +0530

    making backend side address part

---
+++ server/controllers/shop/address-controllers.js
@@ -0,0 +1,49 @@
+
+
+export const addAddress=async(req,res)=>{
+    try {
+        
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            success:false,
+            message: "Error"
+        })
+    }
+}
+
+export const fetchAllAddress=async(req,res)=>{
+    try {
+        
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            success:false,
+            message: "Error"
+        })
+    }
+}
+
+export const editAddress=async(req,res)=>{
+    try {
+        
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            success:false,
+            message: "Error"
+        })
+    }
+}
+
+export const deleteAddress=async(req,res)=>{
+    try {
+        
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            success:false,
+            message: "Error"
+        })
+    }
+}
