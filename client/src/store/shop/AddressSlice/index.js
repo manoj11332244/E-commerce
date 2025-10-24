3b038088811cded385fa3890762971e7bcf6c8a6
Manoj <sahilku770@gmail.com>
2025-10-24 11:38:33 +0530

    redux address

--- client/src/store/shop/AddressSlice/index.js
+++ client/src/store/shop/AddressSlice/index.js
@@ -9,6 +9,24 @@ export const addNewAddress=createAsyncThunk('/addresses/addNewAddress',async(for
     return response.data;
 })
 
+
+export const fetchAllAddress=createAsyncThunk('/addresses/fetchAllAddress',async(userId)=>{
+    const response=await axios.get(`http:localhost:8000/api/shop/address/get/${userId}`)
+    return response.data;
+})
+
+
+export const editAddress=createAsyncThunk('/addresses/editAddress',async({userId,addressId,formData})=>{
+    const response=await axios.put(`http:localhost:8000/api/shop/address/update/${userId}/${addressId}`,formData)
+    return response.data;
+})
+
+
+export const deleteAddress=createAsyncThunk('/addresses/deleteAddress',async({userId,addressId})=>{
+    const response=await axios.delete(`http:localhost:8000/api/shop/address/delete/${userId}/${addressId}`)
+    return response.data;
+})
+
 const addressSlice=createSlice({
     name:"address",
     initialState:{
@@ -19,6 +37,6 @@ const addressSlice=createSlice({
 
     },
     extraReducers:(builder)=>{
-        builder.addbu
+        // builder.addCase(addNewAddress)
     }
 })
